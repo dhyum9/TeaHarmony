2d46ecadfbd225dd0573751c3e53dc7534d52a82
Danny Yum <dhyum9@gmail.com>
2023-10-03 14:37:10 -0600

    Initialize tea seeder file

---
+++ app/seeds/teas.py
@@ -0,0 +1,31 @@
+from app.models import db, User, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+def seed_teas():
+    tea1 = User(
+        username='Demo', email='demo@aa.io', password='password')
+    tea2 = User(
+        username='marnie', email='marnie@aa.io', password='password')
+    tea3 = User(
+        username='bobbie', email='bobbie@aa.io', password='password')
+
+    db.session.add(demo)
+    db.session.add(marnie)
+    db.session.add(bobbie)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_teas():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.teas RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM teas"))
+
+    db.session.commit()
