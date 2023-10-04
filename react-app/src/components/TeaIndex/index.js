9aeba5542fdd8bdb75aeb32d5b665a115210715d
Danny Yum <dhyum9@gmail.com>
2023-10-04 15:20:16 -0600

    Initialize TeaIndex component

---
+++ react-app/src/components/TeaIndex/index.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+export const TeaIndex = () => {
+  const dispatch = useDispatch();
+
+  const getTeas = useSelector(
+    (state) => state.teas.allTeas
+  );
+
+  const teas = Object.values(getTeas);
+
+  useEffect(() => {
+    dispatch(thunkGetTeas());
+  }, [dispatch]);
+
+  if (!teas.length) return null;
+
+  // const showAlert = () => {
+  //   window.alert("Feature Coming Soon");
+  // };
+
+  return (
+    <div>
+      {teas.map((tea) => (
+        <TeaTile key={tea.id} tea={tea} />
+      ))}
+    </div>
+  );
+};
