9f308edb58acfe496261392e6a4727b5adc9da02
Danny Yum <dhyum9@gmail.com>
2023-10-04 10:37:26 -0600

    Add GET current user route

--- app/api/tea_routes.py
+++ app/api/tea_routes.py
@@ -5,21 +5,28 @@ from app.models import Tea
 tea_routes = Blueprint('teas', __name__)
 
 
-@user_routes.route('/')
-@login_required
-def users():
+@tea_routes.route('/')
+def get_all_teas():
     """
-    Query for all users and returns them in a list of user dictionaries
+    Returns a list of all teas in dictionary form
     """
-    users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
+
+    teas = Tea.query.all()
+    # reviews = Review.query.all()
+
+    teas_list = [tea.to_dict() for tea in teas]
+    # all_review_list = [review.to_dict() for review in reviews]
 
 
-@user_routes.route('/<int:id>')
-@login_required
-def user(id):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
-    user = User.query.get(id)
-    return user.to_dict()
+    # for restaurant_obj in all_restaurant_list:
+    #     # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>", restaurant_obj.review)
+    #     restaurant_reviews = [ review for review in all_review_list if review["restaurant_id"] == restaurant_obj["id"] ]
+    #     sum_stars = 0
+    #     for restaurant_review in restaurant_reviews:
+    #         sum_stars += restaurant_review["stars"]
+    #     if sum_stars > 0:
+    #         avg_rating = sum_stars / len(restaurant_reviews)
+    #         restaurant_obj["avg_rating"] = avg_rating
+    #         restaurant_obj["num_reviews"] = len(restaurant_reviews)
+
+    return teas_list
