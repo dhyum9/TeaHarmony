8bc04068bc4cfa2d7621b0adc48c3fa86b7c1474
Danny Yum <dhyum9@gmail.com>
2023-10-03 18:05:12 -0600

    Add tasting notes seeder file to __init__ in seeds dir

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .teas import seed_teas, undo_teas
+from .tastingnotes import seed_tasting_notes, undo_tasting_notes
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,16 +18,19 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_tasting_notes
         undo_teas()
         undo_users()
     seed_users()
     seed_teas()
+    seed_tasting_notes
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_tasting_notes
     undo_teas()
     undo_users()
     # Add other undo functions here
