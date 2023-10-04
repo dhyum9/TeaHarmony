e0afa945c9053b2f32dea5fee9d3ca37725b3c68
Danny Yum <dhyum9@gmail.com>
2023-10-04 16:57:48 -0600

    Initialize HomePage component and put Teas in Nav bar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import TeaIndex from "./components/TeaIndex";
 import TeaDetails from "./components/TeaDetails";
+import HomePage from "./components/HomePage";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,6 +21,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/" >
+            <HomePage />
+          </Route>
           <Route path="/login" >
             <LoginFormPage />
           </Route>
---
+++ react-app/src/components/HomePage/index.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+
+  return (
+    <div>
+      <h1>Welcome to TeaHarmony</h1>
+    </div>
+  );
+};
+
+export default HomePage;
