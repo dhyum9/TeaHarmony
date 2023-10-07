e3cebb4280180ce59c997176f21b83044bbd27dd
Danny Yum <dhyum9@gmail.com>
2023-10-07 17:57:55 -0600

    Wip: Nav bar CSS

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import { useHistory } from 'react-router';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
+	const history = useHistory();
+
+  const goToHome = () => {
+    history.push(`/`);
+  };
+
+	const goToTeaIndex = () => {
+    history.push(`/teas`);
+  };
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			<li>
-				<NavLink exact to='/teas'>Teas</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<div className='nav-bar'>
+			<div className='nav-bar-left-section'>
+				<div className='nav-bar-logo' onClick={goToHome}>
+					<i class="fa-solid fa-mug-hot"></i>
+					<div>TeaHarmony</div>
+				</div>
+				<div onClick={goToHome}>
+					Home
+				</div>
+				<div onClick={goToTeaIndex}>
+					Teas
+				</div>
+			</div>
+			<div className='nav-bar-right-section'>
+				{isLoaded && (
+					<div>
+						<ProfileButton user={sessionUser} />
+					</div>
+				)}
+			</div>
+		</div>
 	);
 }
 
