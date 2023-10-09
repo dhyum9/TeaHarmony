855c99d59ed994645effa25b9b23b0ac5c483bd7
Danny Yum <dhyum9@gmail.com>
2023-10-09 16:43:11 -0600

    Finish login modal CSS

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -22,35 +22,37 @@ function LoginFormModal() {
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
+    <div className="login-form-modal">
+      <p>Log In</p>
+      <form className='login-form' onSubmit={handleSubmit}>
+        {/* <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
-        </ul>
-        <label>
+        </ul> */}
+        <label for='credential-field'>
           Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
         </label>
-        <label>
+        <input
+          id='credential-field'
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label for='password-field'>
           Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
         </label>
-        <button type="submit">Log In</button>
+        <input
+          id='password-field'
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button className='login-submit' type="submit">LOG IN</button>
       </form>
-    </>
+    </div>
   );
 }
 
