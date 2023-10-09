59eaf9d0f59d2ea079a91bda1fa04cfe47d65017
Danny Yum <dhyum9@gmail.com>
2023-10-09 09:17:48 -0600

    wip: Navigation Bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import { useHistory } from 'react-router';
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -21,27 +24,46 @@ function Navigation({ isLoaded }){
 		<div className='nav-bar'>
 			<div className='nav-bar-left-section'>
 				<div className='nav-bar-logo' onClick={goToHome}>
-					<i class="fa-solid fa-mug-hot"></i>
+					<i className="fa-solid fa-mug-hot"></i>
 					<div>TeaHarmony</div>
 				</div>
-				<div onClick={goToHome}>
-					Home
-				</div>
-				<div onClick={goToTeaIndex}>
-					Teas
-				</div>
-				<div>
-					Places
-				</div>
-				<div>
-					Explore
+				<div className='nav-bar-options'>
+					<div onClick={goToHome}>
+						Home
+					</div>
+					<div onClick={goToTeaIndex}>
+						Teas
+					</div>
+					<div>
+						Places
+					</div>
+					<div>
+						Explore
+					</div>
 				</div>
 			</div>
 			<div className='nav-bar-right-section'>
-				{isLoaded && (
+				{isLoaded && sessionUser && (
 					<div>
 						<ProfileButton user={sessionUser} />
 					</div>
+				)}
+				{isLoaded && !sessionUser && (
+					<>
+						<div> Learn More </div>
+						<div>
+							<OpenModalButton
+								buttonText="Log In"
+								modalComponent={<LoginFormModal />}
+							/>
+						</div>
+						<div>
+							<OpenModalButton
+								buttonText="Sign Up"
+								modalComponent={<SignupFormModal />}
+							/>
+						</div>
+					</>
 				)}
 			</div>
 		</div>
