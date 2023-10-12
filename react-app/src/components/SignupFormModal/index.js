e069fd6338a3f02c330b1439413d32e37bd32fd5
Danny Yum <dhyum9@gmail.com>
2023-10-12 14:11:30 -0600

    Give each signup input height so errors do not change format of form. Finish home page signupmodal

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -55,85 +55,110 @@ function SignupFormModal({modalType}) {
 			</div>
 			)}
 			<form className='signup-form' onSubmit={handleSubmit}>
-				<div className='signup-label-container'>
-					<label for='signup-email-field'>Email</label>
-					{errors.email && (
-						<div className='signup-error'>{errors.email}</div>
+
+				<div className={appendModalTypetoClass('signup-section-container')}>
+					<div className={appendModalTypetoClass('signup-label-container')}>
+						<label for='signup-email-field'>Email</label>
+						{errors.email && modalType === 'signup-nav' && (
+							<div className='signup-error'>{errors.email}</div>
 						)}
-				</div>
-				<input
-					className='signup-field'
-					id='signup-email-field'
-					type="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					required
+					</div>
+					<input
+						className={appendModalTypetoClass('signup-field')}
+						id='signup-email-field'
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
 					/>
-				<div className='signup-label-container'>
-					<label for='signup-username-field'>Username</label>
-					{errors.username && (
+					{errors.email && modalType === 'signup-home' && (
+						<div className='signup-error'>{errors.email}</div>
+					)}
+				</div>
+
+				<div className={appendModalTypetoClass('signup-section-container')}>
+					<div className={appendModalTypetoClass('signup-label-container')}>
+						<label for='signup-username-field'>Username</label>
+						{errors.username && modalType === 'signup-nav' && (
+							<div className='signup-error'>{errors.username}</div>
+						)}
+					</div>
+					<input
+						className={appendModalTypetoClass('signup-field')}
+						id='signup-username-field'
+						type="text"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						required
+					/>
+					{errors.username && modalType === 'signup-home' && (
 						<div className='signup-error'>{errors.username}</div>
 					)}
 				</div>
-				<input
-					className='signup-field'
-					id='signup-username-field'
-					type="text"
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-					required
-				/>
-				<div className='signup-label-container'>
-					<label for='signup-password-field'>Password</label>
-					{errors.password && (
-					<div className='signup-error'>{errors.password}</div>
-					)}
-				</div>
-				<input
-					className='signup-field'
-					id='signup-password-field'
-					type="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					required
-				/>
-				<div className='signup-label-container'>
-					<label for='signup-confirm-password-field'>Confirm Password</label>
-					{errors.confirmpassword && (
-					<div className='signup-error'>{errors.confirmpassword}</div>
-					)}
-				</div>
+
+				<div className={appendModalTypetoClass('signup-section-container')}>
+					<div className={appendModalTypetoClass('signup-label-container')}>
+						<label for='signup-password-field'>Password</label>
+						{errors.password && modalType === 'signup-nav' && (
+						<div className='signup-error'>{errors.password}</div>
+						)}
+					</div>
 					<input
-						className='signup-field'
+						className={appendModalTypetoClass('signup-field')}
+						id='signup-password-field'
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+					{errors.password && modalType === 'signup-home' && (
+						<div className='signup-error'>{errors.password}</div>
+					)}
+				</div>
+
+				<div className={appendModalTypetoClass('signup-section-container')}>
+					<div className={appendModalTypetoClass('signup-label-container')}>
+						<label for='signup-confirm-password-field'>Confirm Password</label>
+						{errors.confirmpassword && modalType === 'signup-nav' && (
+						<div className='signup-error'>{errors.confirmpassword}</div>
+						)}
+					</div>
+					<input
+						className={appendModalTypetoClass('signup-field')}
 						id='signup-confirm-password-field'
 						type="password"
 						value={confirmPassword}
 						onChange={(e) => setConfirmPassword(e.target.value)}
 						required
 					/>
-					<div className='signup-bottom-row'>
-						<div className='signup-bottom-button-row'>
-							{modalType === 'signup-nav' && (
-								<div>Already have an account?
-									<OpenModalButton
-									buttonText="Log In"
-									modalComponent={<LoginFormModal />}
-									buttonType="signup-modal-option"
-									/>
-								</div>
-							)}
-							<button className={appendModalTypetoClass('signup-submit')} type="submit">SIGN UP</button>
-						</div>
+					{errors.confirmpassword && modalType === 'signup-home' && (
+						<div className='signup-error'>{errors.confirmpassword}</div>
+					)}
+				</div>
+
+				<div className='signup-bottom-row'>
+					<div className='signup-bottom-button-row'>
 						{modalType === 'signup-nav' && (
-							<div className='signup-tips'>
-								<div className='signup-tip-label'>Sign Up Tips:</div>
-								<div className='signup-tip'>All fields are required.</div>
-								<div className='signup-tip'>Must use a valid email.</div>
-								<div className='signup-tip'>Username must be 4 characters or more.</div>
-								<div className='signup-tip'>Password must be 6 characters or more.</div>
+							<div>Already have an account?
+								<OpenModalButton
+								buttonText="Log In"
+								modalComponent={<LoginFormModal />}
+								buttonType="signup-modal-option"
+								/>
 							</div>
 						)}
+						<button className={appendModalTypetoClass('signup-submit')} type="submit">SIGN UP</button>
 					</div>
+					{modalType === 'signup-nav' && (
+						<div className='signup-tips'>
+							<div className='signup-tip-label'>Sign Up Tips:</div>
+							<div className='signup-tip'>All fields are required.</div>
+							<div className='signup-tip'>Must use a valid email.</div>
+							<div className='signup-tip'>Username must be 4 characters or more.</div>
+							<div className='signup-tip'>Password must be 6 characters or more.</div>
+						</div>
+					)}
+				</div>
 			</form>
 		</div>
 	);
