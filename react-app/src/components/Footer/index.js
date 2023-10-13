44dbc7d2af2cc6fe84095b92d37107690a213129
Danny Yum <dhyum9@gmail.com>
2023-10-13 10:46:45 -0600

    Fix footer to be at bottom of screen

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,21 +5,23 @@ import LearnMoreModal from '../LearnMoreModal';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='footer-left'>
-        TeaHarmony 2023
-      </div>
-      <div className='footer-right'>
-        <a href='https://github.com/dhyum9/TeaHarmony' target='_blank' className='footer-right-option'>
-          <img className='footer-github-logo' src={GithubLogo}></img>
-          <div>Github</div>
-        </a>
-        <div className='footer-right-option'>
-          <OpenModalButton
-            buttonText="Learn More"
-            modalComponent={<LearnMoreModal />}
-            buttonType="footer-option"
-          />
+    <div className='footer-container'>
+      <div className='footer'>
+        <div className='footer-left'>
+          TeaHarmony 2023
+        </div>
+        <div className='footer-right'>
+          <a href='https://github.com/dhyum9/TeaHarmony' target='_blank' className='footer-right-option'>
+            <img className='footer-github-logo' src={GithubLogo}></img>
+            <div>Github</div>
+          </a>
+          <div className='footer-right-option'>
+            <OpenModalButton
+              buttonText="Learn More"
+              modalComponent={<LearnMoreModal />}
+              buttonType="footer-option"
+            />
+          </div>
         </div>
       </div>
     </div>
