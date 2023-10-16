670c10c47c131f6cdecb5f99cd6849a0763e49a4
Danny Yum <dhyum9@gmail.com>
2023-10-16 08:58:56 -0600

    Add more text to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -8,7 +8,30 @@ const Footer = () => {
     <div className='footer-container'>
       <div className='footer'>
         <div className='footer-left'>
-          TeaHarmony 2023
+          <div>
+            TeaHarmony 2023
+          </div>
+          <div>
+            Python
+          </div>
+          <div>
+            Flask
+          </div>
+          <div>
+            JavaScript
+          </div>
+          <div>
+            React
+          </div>
+          <div>
+            Redux
+          </div>
+          <div>
+            CSS 3
+          </div>
+          <div>
+            HTML 5
+          </div>
         </div>
         <div className='footer-right'>
           <a href='https://github.com/dhyum9/TeaHarmony' target='_blank' className='footer-right-option'>
